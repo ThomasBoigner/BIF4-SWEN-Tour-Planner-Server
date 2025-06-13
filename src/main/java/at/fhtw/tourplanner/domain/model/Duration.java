d1d3702e74eab5a4ee966773ee1bc8823228ffbb
Thomas Boigner <thomasboigner@outlook.com>
2025-06-13 19:23:59 +0200

    Fix bugs
    - Add two way relationship to TourEntity and TourLogEntity
    - Fix Duration calculation

--- src/main/java/at/fhtw/tourplanner/domain/model/Duration.java
+++ src/main/java/at/fhtw/tourplanner/domain/model/Duration.java
@@ -4,18 +4,26 @@ import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Slf4j
-@Builder
 public record Duration(LocalDateTime startTime, LocalDateTime endTime, long duration) {
     public Duration {
+        Objects.requireNonNull(startTime, "startTime must not be null");
+        Objects.requireNonNull(endTime, "endTime must not be null");
         if (endTime.isBefore(startTime)) {
-            throw new IllegalArgumentException("Start time must be before end time");
+            throw new IllegalArgumentException("start time must be before end time");
         }
     }
 
+    @Builder
     public Duration(LocalDateTime startTime, LocalDateTime endTime) {
-        this(startTime, endTime, java.time.Duration.between(startTime, endTime).toMinutes());
+        this(startTime,
+                endTime,
+                startTime != null && endTime != null
+                        ? java.time.Duration.between(startTime, endTime).toMinutes()
+                        : 0
+        );
         log.debug("Created duration: {}", this);
     }
 }
