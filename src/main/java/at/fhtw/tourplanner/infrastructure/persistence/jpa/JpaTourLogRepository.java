9f3b07815defa3a5c3eaca9b46764759931905a9
Thomas Boigner <59614919+ThomasBoigner@users.noreply.github.com>
2025-06-28 17:51:13 +0200

    Dockerize application

--- src/main/java/at/fhtw/tourplanner/infrastructure/persistence/jpa/JpaTourLogRepository.java
+++ src/main/java/at/fhtw/tourplanner/infrastructure/persistence/jpa/JpaTourLogRepository.java
@@ -3,13 +3,11 @@ package at.fhtw.tourplanner.infrastructure.persistence.jpa;
 import at.fhtw.tourplanner.domain.model.*;
 import at.fhtw.tourplanner.domain.util.Page;
 import at.fhtw.tourplanner.infrastructure.persistence.jpa.mapper.TourLogEntityMapper;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,58 +15,6 @@ import java.util.Optional;
 
 @Repository
 public class JpaTourLogRepository implements TourLogRepository {
-    private Tour tour1 = Tour.builder()
-            .name("Tour 1")
-            .description("This tour is awesome")
-            .from(Address.builder()
-                    .streetName("Austria")
-                    .city("Deutsch Wagram")
-                    .zipCode(2232)
-                    .streetName("Radetzkystraße")
-                    .streetNumber("2-6")
-                    .country("Austria")
-                    .latitude(48.29903)
-                    .longitude(16.564899)
-                    .build())
-            .to(Address.builder()
-                    .streetName("Austria")
-                    .city("Strasshof an der Nordbahn")
-                    .zipCode(2231)
-                    .streetName("Billroth-Gasse")
-                    .streetNumber("5")
-                    .country("Austria")
-                    .latitude(48.317181)
-                    .longitude(16.64259)
-                    .build())
-            .transportType(TransportType.BIKE)
-            .distance(20)
-            .estimatedTime(120)
-            .build();
-
-    private TourLog tourlog1 = TourLog.builder()
-            .tour(tour1)
-            .duration(Duration.builder()
-                    .startTime(LocalDateTime.of(2025, 1, 1, 12, 0, 0))
-                    .endTime(LocalDateTime.of(2025, 1, 1, 13, 0, 0))
-                    .build())
-            .comment("What a nice tour!")
-            .difficulty(new Difficulty(3))
-            .distance(2)
-            .rating(new Rating(5))
-            .build();
-
-    private TourLog tourlog2 = TourLog.builder()
-            .tour(tour1)
-            .duration(Duration.builder()
-                    .startTime(LocalDateTime.of(2025, 2, 1, 8, 0, 0))
-                    .endTime(LocalDateTime.of(2025, 2, 1, 10, 0, 0))
-                    .build())
-            .comment("Super cool!")
-            .difficulty(new Difficulty(2))
-            .distance(1.5)
-            .rating(new Rating(4))
-            .build();
-
     private final TourLogEntityRepository tourLogEntityRepository;
     private final TourLogEntityMapper tourLogEntityMapper;
 
@@ -115,12 +61,5 @@ public class JpaTourLogRepository implements TourLogRepository {
     @Override
     public void deleteTourLogById(TourLogId id) {
         tourLogEntityRepository.deleteTourLogEntityById(id.id());
-    }
-
-    @PostConstruct
-    void initDB() {
-        save(tourlog1);
-
-        save(tourlog2);
     }
 }
