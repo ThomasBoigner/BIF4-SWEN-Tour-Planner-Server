c42bc4eac66c522953d3ff45e13497cae8979a6f
Thomas Boigner <thomasboigner@outlook.com>
2025-06-28 12:40:06 +0200

    Dockerize application

--- src/main/java/at/fhtw/tourplanner/infrastructure/persistence/jpa/JpaTourRepository.java
+++ src/main/java/at/fhtw/tourplanner/infrastructure/persistence/jpa/JpaTourRepository.java
@@ -3,7 +3,6 @@ package at.fhtw.tourplanner.infrastructure.persistence.jpa;
 import at.fhtw.tourplanner.domain.model.*;
 import at.fhtw.tourplanner.domain.util.Page;
 import at.fhtw.tourplanner.infrastructure.persistence.jpa.mapper.TourEntityMapper;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -15,62 +14,6 @@ import java.util.Optional;
 
 @Repository
 public class JpaTourRepository implements TourRepository {
-    private Tour tour2 = Tour.builder()
-            .name("Tour 2")
-            .description("This tour is awesome")
-            .from(Address.builder()
-                    .streetName("Austria")
-                    .city("Deutsch Wagram")
-                    .zipCode(2232)
-                    .streetName("Radetzkystraße")
-                    .streetNumber("2-6")
-                    .country("Austria")
-                    .latitude(48.360310)
-                    .longitude(16.600938)
-                    .build())
-            .to(Address.builder()
-                    .streetName("Austria")
-                    .city("Strasshof an der Nordbahn")
-                    .zipCode(2231)
-                    .streetName("Billroth-Gasse")
-                    .streetNumber("5")
-                    .country("Austria")
-                    .latitude(48.567379)
-                    .longitude(16.572229)
-                    .build())
-            .transportType(TransportType.RUNNING)
-            .distance(40)
-            .estimatedTime(240)
-            .build();
-
-    private Tour tour3 = Tour.builder()
-            .name("Tour 3")
-            .description("This tour is awesome")
-            .from(Address.builder()
-                    .streetName("Austria")
-                    .city("Deutsch Wagram")
-                    .zipCode(2232)
-                    .streetName("Radetzkystraße")
-                    .streetNumber("2-6")
-                    .country("Austria")
-                    .latitude(48.384689)
-                    .longitude(16.515160)
-                    .build())
-            .to(Address.builder()
-                    .streetName("Austria")
-                    .city("Strasshof an der Nordbahn")
-                    .zipCode(2231)
-                    .streetName("Billroth-Gasse")
-                    .streetNumber("5")
-                    .country("Austria")
-                    .latitude(48.527710)
-                    .longitude(16.361530)
-                    .build())
-            .transportType(TransportType.BIKE)
-            .distance(30)
-            .estimatedTime(60)
-            .build();
-
     private final TourEntityRepository tourEntityRepository;
     private final TourEntityMapper tourEntityMapper;
 
@@ -113,12 +56,5 @@ public class JpaTourRepository implements TourRepository {
     @Override
     public void deleteTourById(TourId id) {
         tourEntityRepository.deleteTourEntityById(id.id());
-    }
-
-    @PostConstruct
-    void initDB() {
-        save(tour2);
-
-        save(tour3);
     }
 }
